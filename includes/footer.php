4fcd223c925094ba293cf6f3bae201c9b4452f3c
n-mariana <mariana.sgoncalves2010@gmail.com>
2026-05-19 19:24:23 -0300

    Adição do index.php e da pasta includes, com o header e o footer

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<!-- rodape.php -->
+    <hr>
+    <footer>
+        <p>&copy; 2026 - Desenvolvido pela Turma de Informática - Etec Albert Einstein</p>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+  require 'config/conexao.php'; // Puxa o banco
+  require 'includes/header.php'; // Puxa o topo visual
+?>
+
+<h2>Bem-vindo à Agenda</h2>
+<p>Aqui você pode gerenciar seus contatos de forma rápida.</p>
+
+<?php 
+    $sql = "SELECT * FROM produtos ORDER BY id ASC";
+    $stmt = $conexao->prepare($sql);
+    $stmt->execute();
+    $produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo "<h2>Lista de Produtos</h2>";
+
+    if($produtos){
+        foreach ($produtos as $registro){
+            echo "ID: " . $registro['id'] . " | ";
+            echo "Nome: " . $registro['nome'] . " | ";
+            echo "Fabricante: " . $registro['fabricante'] . " | ";
+            echo "Preço: " . $registro['preco'] . " | ";
+            echo "Estoque: " . $registro['estoque']  . "<br>";
+            echo "------------------------------<br>";
+        }
+    }
+    
+    else{
+    echo "A lista de produtos está vazia.";
+    }
+    require 'includes/footer.php'; // Puxa o final do site
+?>
